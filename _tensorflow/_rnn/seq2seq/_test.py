4f07ce1a17da9724384e76579e5fe1d8ffe569c1
=yisun <=>
2018-10-09 16:24:57 +0800

    +sequence2sequence训练模型模型

--- _tensorflow/_rnn/seq2seq/_test.py
+++ _tensorflow/_rnn/seq2seq/_test.py
@@ -7,11 +7,14 @@ Created on 2018--
 @group:data
 """
 import codecs
+import chardet
 
 # 根据中文词汇表, 将翻译结果转换为中文文字
 with codecs.open('./data/zh.vocab', 'r', 'utf-8') as f_vocab:
     trg_vocab = [w.strip() for w in f_vocab.readlines()]
 print(trg_vocab)
 output_ids = [19, 13, 9, 0]
-output_text = [trg_vocab[x] for x in output_ids]
-print(output_text)
+output_text = ''.join([trg_vocab[x] for x in output_ids])
+# print(output_text)
+fenconding = chardet.detect(output_text)
+print(fenconding)
