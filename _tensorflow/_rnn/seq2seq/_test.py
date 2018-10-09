53863752a4385ea26f03ef5e66b9c41065e9bda8
=yisun <=>
2018-10-09 16:37:01 +0800

    +sequence2sequence训练模型模型

--- _tensorflow/_rnn/seq2seq/_test.py
+++ _tensorflow/_rnn/seq2seq/_test.py
@@ -13,7 +13,7 @@ import sys
 # 根据中文词汇表, 将翻译结果转换为中文文字
 with codecs.open('./data/zh.vocab', 'rb', 'utf8') as f_vocab:
     trg_vocab = [w.strip() for w in f_vocab.readlines()]
-print(trg_vocab)
+# print(trg_vocab)
 output_ids = [19, 13, 9, 0]
 output_text = ' '.join([trg_vocab[x] for x in output_ids])
 # print(type(output_text))
