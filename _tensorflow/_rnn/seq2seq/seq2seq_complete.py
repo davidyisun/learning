63f9df1e0196c9fc8550a1254c328375c4b23bc2
=yisun <=>
2018-10-09 13:12:34 +0800

    +sequence2sequence训练模型模型

--- _tensorflow/_rnn/seq2seq/seq2seq_complete.py
+++ _tensorflow/_rnn/seq2seq/seq2seq_complete.py
@@ -10,6 +10,13 @@ Created on 2018-10-08
 import tensorflow as tf
 import codecs
 import tqdm
+import sys
+
+# 0.设置test输入
+class input():
+    flags = tf.flags
+    flags.DEFINE_string('en_sentence', 'this is a book.', 'str: the sentence needed to translate.')
+    FLags = flags.FLAGS
 
 
 # 1.参数设置。
@@ -35,8 +42,7 @@ class parameters():
 
 
 class predict_parameters():
-    def __init__(self):
-        self.checkpoint_path = './model/seq2seq_ckpt-9000'  # 读取checkpoint的路径。9000表示是训练程序在第9000步保存的checkpoint
+        checkpoint_path = './model/seq2seq_ckpt-9000'  # 读取checkpoint的路径。9000表示是训练程序在第9000步保存的checkpoint
         # 模型参数 必须 和训练模型参数保持一致
         hidden_size = 1024  # lstm的隐藏层规模
         num_layers = 2  # 深层循环神经网络中lstm结构的层数
@@ -52,6 +58,7 @@ class predict_parameters():
 
 
 para = parameters()
+para_predict = predict_parameters()
 
 
 class vocab():
@@ -217,6 +224,58 @@ class NMTModel(object):
         return cost_per_token, train_op
 
 
+    def inference(self, src_input):  # 推理
+        # 虽然输入只有一个句子，但因为dynamic_rnn要求输入是batch的形式，因此这里将输入句子整理为大小为1的batch
+        src_size = tf.convert_to_tensor([len(src_input)], dtype=tf.int32)
+        src_input = tf.convert_to_tensor([src_input], dtype=tf.int32)
+        src_emb = tf.nn.embedding_lookup(self.src_embedding, src_input)
+
+        # 使用dynamic_rnn构造编码器. 这一步与训练时相同。
+        with tf.variable_scope('encoder'):
+            enc_outputs, enc_state = tf.nn.dynamic_rnn(cell=self.enc_cell,
+                                                       inputs=src_emb,
+                                                       sequence_length=src_size,
+                                                       dtype=tf.float32)
+        # 设置解码的最大步数。这是为了避免在极端情况出现无限循环的问题
+        max_dec_len=100
+
+        with tf.variable_scope('decoder/rnn/multi_rnn_cell'):
+            # 使用一个变长的tensorArray来存储生成的句子。
+            init_array = tf.TensorArray(dtype=tf.int32, size=0, dynamic_size=True, clear_after_read=False)
+            # 填入第一个单词<sos>作为解码器的输入
+            init_array = init_array.write(0, para_predict.sos_id)
+            # 构建初始的循环状态。循环状态包含循环神经网络的隐藏状态，保存生成句子的TensorArray,以及记录解码步数的一个整数step
+            init_loop_var = (enc_state, init_array, 0)
+            # tf.while_loop的循环条件: 循环直到解码器输出<eos>，或者达到最大步数为止
+            def continue_loop_condition(state, trg_ids, step):
+                return tf.reduce_all(tf.logical_and(tf.not_equal(trg_ids.read(step),
+                                                                 para_predict.eos_id),
+                                                    tf.less(step, max_dec_len)
+
+                ))
+            def loop_body(state, trg_ids, step):  # trg_ids 为上一个词的id
+                # 读取最后一步输出的单词， 并读取其词向量。
+                trg_input = [trg_ids.read(step)]
+                trg_emb = tf.nn.embedding_lookup(self.trg_embedding,
+                                                 trg_input)
+                # 这里不适用dynamic_rnn, 而是直接调用dec_cell向前计算一步。
+                dec_outputs, next_state = self.dec_cell.call(inputs=trg_emb,
+                                                             state=state)
+                # 计算每个可能的输出单词对应的logit， 并选取logit值最大的单词作为这一步的id而输出。
+                output = tf.reshape(dec_outputs, [-1, para_predict.hidden_size])
+                logits = (tf.matmul(output, self.softmax_weight)+self.softmax_bias)
+                next_id = tf.argmax(logits, axis=1, output_type=tf.int32)
+                # 将这一步输出的单词写入循环状态的trg_ids中
+                trg_ids = trg_ids.write(step+1, next_id[0])
+                return next_state, trg_ids, step+1
+
+            # 执行tf.while_loop, 返回最终状态
+            state, trg_ids, step = tf.while_loop(cond=continue_loop_condition,
+                                                 body=loop_body,
+                                                 loop_vars=init_loop_var)
+            return trg_ids.stack()
+
+
 # 4.训练过程和主函数。
 def run_epoch(session, cost_op, train_op, saver, step):
     """
@@ -244,7 +303,8 @@ def run_epoch(session, cost_op, train_op, saver, step):
     return step
 
 
-def main():
+# 训练主函数
+def train_main():
     # 定义初始化函数。
     initializer = tf.random_uniform_initializer(-0.05, 0.05)
 
@@ -279,7 +339,46 @@ def main():
             pbar.update(1)
 
 
+def predict_main():
+    # 定义训练用的循环神经网络模型
+    with tf.variable_scope('nmt_model', reuse=None):
+        model = NMTModel()
+
+    # 定义个测试句子
+    flags = input()
+    test_en_text = flags.FLags.en_sentence
+    # 根据英文词汇表, 将句子转为单词id
+    with codecs.open(predict_parameters.src_vocab, 'r', 'utf-8') as f_vovab:
+        src_vocab = [w.strip() for w in f_vovab.readlines()]
+        if '<eos>' not in src_vocab:
+            src_vocab.append('<eos>')
+        src_id_dict = dict((src_vocab[x], x) for x in range(len(src_vocab)))
+    test_en_ids = [(src_id_dict[token] if token in src_id_dict else src_id_dict['<unk>']) for token in test_en_text.split()]
+
+    print(test_en_ids)
+
+    # 建立解码所需的计算图
+    output_op = model.inference(test_en_ids)
+    with tf.Session() as sess:
+        saver = tf.train.Saver()
+        saver.restore(sess, predict_parameters.checkpoint_path)  # 加载模型
+
+    # 读取翻译结果
+        output_ids = sess.run(output_op)
+
+    # 根据中文词汇表, 将翻译结果转换为中文文字
+    with codecs.open(para_predict.trg_vocab, 'r', 'utf-8') as f_vocab:
+        trg_vocab = [w.strip() for w in f_vovab.readlines()]
+    output_text = ''.join([trg_vocab[x] for x in output_ids])
+
+    # 输出翻译结果
+    print(output_text.encode('utf8').decode(sys.stdout.encoding))
+
+
+
+
 if __name__ == '__main__':
     # get_vocab(path='./data/zh.vocab')
     # MakeDataset(file_path=para.trg_train_data)
-    main()
+    # train_main()
+    predict_main()
